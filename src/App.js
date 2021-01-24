d19ee30fc471f379aca7c510976f2251d2f50ec6
hk1510 <hkthetinkerer@gmail.com>
2021-01-23 22:21:02 -0800

    modified App.css

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import './App.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
 
 import Sort from './Components/Sort'
 
